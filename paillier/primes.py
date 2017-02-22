11f1fd4c3e8fc889068b7815a7164e51d98bfa67
Leon R. Chen <Metaception@users.noreply.github.com>
2017-02-22 17:58:03 -0500

    Update primes.py

--- paillier/primes.py
+++ paillier/primes.py
@@ -4,15 +4,15 @@ import sys
 def ipow(a, b, n):
     """calculates (a**b) % n via binary exponentiation, yielding itermediate
        results as Rabin-Miller requires"""
-    A = a = long(a % n)
+    A = a = int(a % n)
     yield A
-    t = 1L
+    t = 1
     while t <= b:
         t <<= 1
 
     # t = 2**k, and t > b
     t >>= 2
-    
+
     while t:
         A = (A * A) % n
         if t & b:
@@ -22,7 +22,7 @@ def ipow(a, b, n):
 
 def rabin_miller_witness(test, possible):
     """Using Rabin-Miller witness test, will return True if possible is
-       definitely not prime (composite), False if it may be prime."""    
+       definitely not prime (composite), False if it may be prime."""
     return 1 not in ipow(test, possible-1, possible)
 
 smallprimes = (2,3,5,7,11,13,17,19,23,29,31,37,41,43,
@@ -41,16 +41,16 @@ def is_probably_prime(possible, k=None):
             return True
         if possible % i == 0:
             return False
-    for i in xrange(k):
+    for i in range(k):
         test = random.randrange(2, possible - 1) | 1
         if rabin_miller_witness(test, possible):
             return False
     return True
 
 def generate_prime(bits, k=None):
-    """Will generate an integer of b bits that is probably prime 
-       (after k trials). Reasonably fast on current hardware for 
-       values of up to around 512 bits."""    
+    """Will generate an integer of b bits that is probably prime
+       (after k trials). Reasonably fast on current hardware for
+       values of up to around 512 bits."""
     assert bits >= 8
 
     if k is None:
@@ -60,4 +60,3 @@ def generate_prime(bits, k=None):
         possible = random.randrange(2 ** (bits-1) + 1, 2 ** bits) | 1
         if is_probably_prime(possible, k):
             return possible
-
